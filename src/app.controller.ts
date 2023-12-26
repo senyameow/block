724942353ef08c28c209f3a128e4ddad0b4c028f
senyameow <arseniyxd@gmail.com>
2023-12-26 23:41:48 +0700

    swagger types

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+
+class ForSwagger {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: ForSwagger
+  })
+  getHello(): ForSwagger {
+    return { message: this.appService.getHello() }
   }
 }
