f0cac594c4d15e234bcdf4991ee9b6374d0dd95e
senyameow <arseniyxd@gmail.com>
2023-12-29 23:19:41 +0700

    types

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,34 @@
-import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { GetSessionInfoDto, SignInBodyDto, SignUpBodyDto } from './dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
 
     @Post('sign-up')
-    signUp() {
-
+    @ApiCreatedResponse()
+    signUp(@Body() body: SignUpBodyDto) {
+        return null
     }
 
     @Post('sign-in')
+    @ApiOkResponse()
     @HttpCode(HttpStatus.OK)
-    signIn() {
-
+    signIn(@Body() body: SignInBodyDto) {
+        return null
     }
 
     @Post('sign-out')
+    @ApiOkResponse()
     @HttpCode(HttpStatus.OK)
     signOut() {
-
     }
 
     @Get('session')
+    @ApiOkResponse({
+        type: GetSessionInfoDto
+    })
     getSession() {
-
+        return null
     }
 }
