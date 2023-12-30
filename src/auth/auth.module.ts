889648042c7d9e1cd6318b20f4509e5ea7d86d44
senyameow <arseniyxd@gmail.com>
2023-12-30 23:54:10 +0700

    import JWT-module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,17 @@ import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
 import { CookieService } from './cookie.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  imports: [UserModule],
+  imports: [UserModule, JwtModule.register({
+    global: true,
+    secret: process.env.SECRET_JWT,
+    signOptions: {
+      expiresIn: '1d'
+    }
+  })],
   providers: [AuthService, PasswordService, CookieService]
 })
 export class AuthModule { }
