98d61e75bce9e141b6d5cee8690e63c5f985bc37
senyameow <arseniyxd@gmail.com>
2023-12-30 23:43:25 +0700

    fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
 import { CookieService } from './cookie.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [AuthController],
+  imports: [UserModule],
   providers: [AuthService, PasswordService, CookieService]
 })
-export class AuthModule {}
+export class AuthModule { }
